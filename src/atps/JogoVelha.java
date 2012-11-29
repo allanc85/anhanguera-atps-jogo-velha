4bb02c4bb441df204da7b4811f9e87d6387f424c
Gabriel Vitor <gvsrepins@gmail.com>
2012-11-29 02:20:48 -0200

    add  metodos controle jogo #5

--- src/atps/JogoVelha.java
+++ src/atps/JogoVelha.java
@@ -3,12 +3,22 @@ package atps;
 import java.io.Serializable;
 
 public class JogoVelha implements Serializable {
+	
     private static final long serialVersionUID = 1L;
     private int idJogadorUm, idJogadorDois; // Jogador 1 = X e Jogador 2 = O
     private String nomeJogadorUm, nomeJogadorDois;
     private char[][] tabuleiro;
-    private int[] jogo;
+    
+    private int jogo; //jogo em andamento
     private int vezJogador;
+
+    /**
+     * armazena o status do jogo
+     * -1 = empate 
+     *  0 = jogo em andamento
+     * >0 = idjogador vencendor
+     */
+    private int[] statusJogo;    
     
     // Construtor
     public JogoVelha() {
@@ -25,8 +35,9 @@ public class JogoVelha implements Serializable {
 
     // Inicializa variáveis responsáveis pelo controle do jogo
     public void InicializaVariaveis() {
-        tabuleiro = new char[3][3];
-        jogo      = new int[3];    //idJogadorVencedor
+        this.tabuleiro 		= new char[3][3];
+        this.statusJogo    	= new int[3];
+        this.jogo 			= 0;
         this.limpaIdJogador();
         this.limpaTabuleiro();
         this.limpaJogo();
@@ -50,7 +61,7 @@ public class JogoVelha implements Serializable {
     // Inicializa Jogo
     private void limpaJogo() {
         for(int i=0;i<3;i++) {
-            this.jogo[i] = 0;
+            this.statusJogo[i] = 0;
         }
     }
 
@@ -108,13 +119,125 @@ public class JogoVelha implements Serializable {
     public char[][] getTabuleiro() {
         return this.tabuleiro;
     }
-
-    // Recebe jogador atual
+ 
+    /**
+     * Recebe jogador atual
+     * @return
+     */
     public int getVezJogador() {
         return this.vezJogador;
     }
-
-    // Valida e realiza jogada
+    
+    /**
+     * Verifica se o jogador é o vencedor
+     * @param idJogador
+     * @return
+     */
+    public boolean isVencedor(int idJogador){
+    	int linha = 0;
+    	
+    	//define o marcador
+    	char marcador = idJogador == this.idJogadorUm ? 'X' : 'O';
+    	
+    	//percorre as  horizontais
+    	for(int i=0;i<3;i++) {
+            for(int j=0;j<3;j++) {
+                if(this.tabuleiro[i][j] == marcador)
+                	linha++;
+            }
+            
+            if(linha==3)
+            	return true;
+            else
+            	linha = 0;           
+        }
+    	
+    	//percorre as verticais
+    	for(int i=0;i<3;i++) {
+            for(int j=0;j<3;j++) {
+                if(this.tabuleiro[j][i] == marcador)
+                	linha++;
+            }
+            
+            if(linha==3)
+            	return true;
+            else
+            	linha = 0;           
+        }
+    	
+    	//percorre a diagonal esquerda
+    	for(int i=0;i<3;i++) {
+    		if(this.tabuleiro[i][i] == marcador)
+            	linha++;
+    	}
+    	
+    	if(linha==3)
+        	return true;
+        else
+        	linha = 0;
+    	
+    	//percorre a diagonal direita
+    	for(int i=0;i<3;i++) {
+    		if(this.tabuleiro[3-i][i] == marcador)
+            	linha++;
+    	}
+    	
+    	if(linha==3)
+        	return true;
+    	
+    	return false;    	    	
+    }       
+    
+    /**
+     * Verifica se o jogo terminou
+     * @return boolean
+     */
+    public boolean isFimJogo(){
+    	for(int i=0;i<3;i++) {
+            for(int j=0;j<3;j++) {
+                if(this.tabuleiro[i][j] == ' ')
+                	return false;
+            }                      
+        }
+    	
+    	return true;
+    }
+    
+    /**
+     * Define o Status do Jogo atual
+     * -1 = Empate 
+     *  0 = Jogo em andamento
+     * >0 = idjogador vencendor
+     * @return
+     */
+    public void setStatusJogo(){
+    	if(this.isVencedor(this.idJogadorUm))
+    		this.statusJogo[this.jogo] =  this.idJogadorUm;
+    	else if(this.isVencedor(this.idJogadorDois))
+    		this.statusJogo[this.jogo] = this.idJogadorDois;
+    	else if(isFimJogo())
+    		this.statusJogo[this.jogo] = -1;
+    	else
+    		this.statusJogo[this.jogo] = 0;    	    	
+    }
+    
+    /**
+     * Retorna o status do jogo
+     * @return
+     */
+    public int getStatusJogo(){
+    	this.setStatusJogo();
+    	return this.statusJogo[this.jogo];
+    }
+    
+    /**
+     * Valida e realiza jogada
+     * @param jogador
+     * @param linha
+     * @param coluna
+     * @param valor
+     * @return bool
+     */
     public boolean setCelula(int jogador, int linha, int coluna, char valor) {
         // Valida vez do jogador
         if(this.vezJogador!=jogador) {
@@ -127,8 +250,11 @@ public class JogoVelha implements Serializable {
 
         // Atribui jogada na célula
         this.tabuleiro[linha-1][coluna-1] = valor;        
+        
         // Próximo jogador
-        this.vezJogador = this.getVezJogador()==this.idJogadorUm?this.idJogadorDois:this.idJogadorUm;
+        this.vezJogador = this.getVezJogador()==this.idJogadorUm
+        				? this.idJogadorDois
+        				: this.idJogadorUm;
 
         return true;
     }
